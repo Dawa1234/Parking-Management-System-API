893c8dd194fa1b9fd61e1885e382507facd9cf61
khangdu <sdawajangbu99@gmail.com>
2022-12-15 09:45:59 +0545

    Database Connection Completed

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("ASD");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+app.use(login);
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect("mongodb://127.0.0.1:27017/PR")
+  .then(() => {
+    console.log("Database Connected successfully");
+  })
+  .catch((err) => console.log(err));
+
+function login(req, res, next) {
+  res.send("Server is working");
+}
+
+app.listen(3000);
